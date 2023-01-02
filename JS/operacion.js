48f03399b4e8e424a6950772f27f8cfba182f492
Kenziefor <myriam.mamani@tecsup.edu.pe>
2023-01-02 17:17:16 -0500

    CalculadoraBasic

---
+++ JS/operacion.js
@@ -0,0 +1,6 @@
+
+function sumar() {
+      var m = parseInt(document.getElememtById('valor1').value);
+      var n = parseInt(document.getElememtById('valor2').value);
+      document.getElementById('Respuesta').innerHTML = m+n;
+}
